8271f941e8f5859428e14f0ba2c6670a368d761b
Gloria <649863555@qq.com>
2019-04-18 09:23:35 +0800

    0418 0.5,1,7,10

--- mmodel/Bayes/params.py
+++ mmodel/Bayes/params.py
@@ -2,8 +2,8 @@ from ..basic_params import parser
 
 def get_params():
     parser.add_argument("--class_num", type=int, default=10)
-    parser.add_argument("--sigma1", type=float, default=-0)
-    parser.add_argument("--sigma2", type=float, default=-6)
-    parser.add_argument("--pi", type=float, default=0.25)
-    parser.add_argument("--samples", type=float, default=5)
+    parser.add_argument("--sigma1", type=float, default=-1)
+    parser.add_argument("--sigma2", type=float, default=-7)
+    parser.add_argument("--pi", type=float, default=0.5)
+    parser.add_argument("--samples", type=float, default=10)
     return parser.parse_args()
